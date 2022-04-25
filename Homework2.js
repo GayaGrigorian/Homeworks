76dd2d02728d56c551e65b14cd6e2a25830603fc
gaya94 <102904157+gaya94@users.noreply.github.com>
2022-04-25 16:19:46 +0400

    Update Homework2.js

--- Homework2.js
+++ Homework2.js
@@ -14,7 +14,6 @@ console.log (result)
 
 /* 2. Given an array consisting from the arrays of numbers (like a two-dimensional array). Find sum of each row and print them as an array */
 
-
 let arr = [[1], [2], [3], [4]]
 let sum = []
 
